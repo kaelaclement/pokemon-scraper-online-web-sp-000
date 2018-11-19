13f99ab80009407395f34f38565fcd5334dd36d9
Kaela Clement <stephanie.kaela@gmail.com>
2018-11-19 04:01:19 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,7 +9,7 @@ class Pokemon
   end
 
   def self.save(id, name, type)
-    @@db.execute("CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT);")
-    @@db.execute("INSERT INTO pokemon (id, name, type) VALUES (?, ?, ?);", id, name, type)
+    @db.execute("CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT);")
+    @db.execute("INSERT INTO pokemon (id, name, type) VALUES (?, ?, ?);", id, name, type)
   end
 end
